8d5463dc280af535a1fd1950eb886f457bddc3fe
Mimi Tran <7123mimit@gmail.com>
2020-08-28 14:50:18 +1000

    insert placeholder controller functions

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,38 @@
 const Category = require('../models/category');
 const async = require('async');
 const validator = require('express-validator');
-const { validationResult } = require('express-validator/check');
+
+// Display detail page for a specific Category.
+exports.category_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category detail: ' + req.params.id);
+};
+
+// Display Category create form on GET.
+exports.category_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category create GET');
+};
+
+// Handle Category create on POST.
+exports.category_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category create POST');
+};
+
+// Display Category delete form on GET.
+exports.category_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category delete GET');
+};
+
+// Handle Category delete on POST.
+exports.category_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category delete POST');
+};
+
+// Display Category update form on GET.
+exports.category_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category update GET');
+};
+
+// Handle category update on POST.
+exports.category_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category update POST');
+};
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,38 @@
 const Item = require('../models/item');
 const async = require('async');
 const validator = require('express-validator');
-const { validationResult } = require('express-validator/check');
+
+// Display detail page for a specific Item.
+exports.item_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Item detail: ' + req.params.id);
+};
+
+// Display Item create form on GET.
+exports.item_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Item create GET');
+};
+
+// Handle Item create on POST.
+exports.item_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Item create POST');
+};
+
+// Display Item delete form on GET.
+exports.item_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Item delete GET');
+};
+
+// Handle Item delete on POST.
+exports.item_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Item delete POST');
+};
+
+// Display Item update form on GET.
+exports.item_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Item update GET');
+};
+
+// Handle item update on POST.
+exports.item_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Item update POST');
+};
