0cce377c2524f361b806fbcf927416173aa68211
Mimi Tran <7123mimit@gmail.com>
2020-08-29 16:38:58 +1000

    add read item detail controller and view

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,10 +1,23 @@
 const Item = require('../models/item');
+const Category = require('../models/category');
 const async = require('async');
 const validator = require('express-validator');
 
 // Display detail page for a specific Item.
-exports.item_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Item detail: ' + req.params.id);
+exports.item_detail = function(req, res, next) {
+    Item.findById(req.params.id)
+    .populate('category')
+    .exec(function(err, result) {
+        if (err) {
+            return next(err);
+        }
+        if (result == null) {
+            let err = new Error('Item not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('item', {title: 'Item detail', item: result});
+    })
 };
 
 // Display Item create form on GET.
