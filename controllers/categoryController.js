6b4b63edd92c94207c21d443bfdc6f0c80d6c4db
Mimi Tran <7123mimit@gmail.com>
2020-08-31 01:06:59 +1000

    add category form

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -30,7 +30,7 @@ exports.category_detail = function(req, res, next) {
 
 // Display Category create form on GET.
 exports.category_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Category create GET');
+    res.render('category_form', {title: 'Create Category'});
 };
 
 // Handle Category create on POST.
