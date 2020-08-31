1a174bc004a98276e5063500bcd31a7f72381307
Mimi Tran <7123mimit@gmail.com>
2020-08-31 11:49:26 +1000

    add category delete page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,21 +2,22 @@ const Category = require('../models/category');
 const Item = require('../models/item');
 const async = require('async');
 const validator = require('express-validator');
+const category = require('../models/category');
 
 // Display detail page for a specific Category.
-exports.category_detail = function(req, res, next) {
+exports.category_detail = function (req, res, next) {
     console.log('id ' + req.params.id);
     async.parallel({
-        category: function(callback) {
+        category: function (callback) {
             Category.findById(req.params.id)
-            .exec(callback)
+                .exec(callback)
         },
-        category_items: function(callback) {
-            Item.find({'category': req.params.id})
-            .exec(callback)
-        } 
-    }, function(err, results) {
-        if (err){
+        category_items: function (callback) {
+            Item.find({ 'category': req.params.id })
+                .exec(callback)
+        }
+    }, function (err, results) {
+        if (err) {
             return next(err);
         }
         if (results.category == null) {
@@ -24,36 +25,56 @@ exports.category_detail = function(req, res, next) {
             err.status = 404;
             return next(err);
         }
-        res.render('category', {title: 'Category list', category: results.category, category_items: results.category_items});
+        res.render('category', { title: 'Category list', category: results.category, category_items: results.category_items });
     }
-)};
+    )
+};
 
 // Display Category create form on GET.
-exports.category_create_get = function(req, res) {
-    res.render('category_form', {title: 'Create Category'});
+exports.category_create_get = function (req, res) {
+    res.render('category_form', { title: 'Create Category' });
 };
 
 // Handle Category create on POST.
-exports.category_create_post = function(req, res) {
+exports.category_create_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Category create POST');
 };
 
 // Display Category delete form on GET.
-exports.category_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Category delete GET');
+exports.category_delete_get = function (req, res) {
+    async.parallel({
+        category: function (callback) {
+            Category.findById(req.params.id)
+                .exec(callback);
+        },
+        category_items: function (callback) {
+            Item.find({ category: req.params.id })
+                .exec(callback);
+        }
+    }, function (err, results) {
+        if (err) {
+            return next(err);
+        }
+        if (results.category == null) {
+            let err = new Error('Category not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('category_delete', { title: 'Delete Category', category: results.category, category_items: results.category_items });
+    })
 };
 
 // Handle Category delete on POST.
-exports.category_delete_post = function(req, res) {
+exports.category_delete_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Category delete POST');
 };
 
 // Display Category update form on GET.
-exports.category_update_get = function(req, res) {
+exports.category_update_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Category update GET');
 };
 
 // Handle category update on POST.
-exports.category_update_post = function(req, res) {
+exports.category_update_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Category update POST');
 };
