1d8337cbd2514c37ba3f3ff042b007a693f7f255
Mimi Tran <7123mimit@gmail.com>
2020-09-02 17:07:58 +1000

    add category update POST

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,7 +5,6 @@ const { body, validationResult } = require('express-validator');
 
 // Display detail page for a specific Category.
 exports.category_detail = function (req, res, next) {
-    console.log('id ' + req.params.id);
     async.parallel({
         category: function (callback) {
             Category.findById(req.params.id)
@@ -43,7 +42,6 @@ exports.category_create_post = [
 
     (req, res, next) => {
         const errors = validationResult(req);
-        console.log(errors.array());
         // if there are errors render the form again with sanitized values/errors messages
         if (!errors.isEmpty()) {
             res.render('category_form', { title: 'Create Category', category: req.body, errors: errors.array() });
@@ -141,6 +139,39 @@ exports.category_update_get = function (req, res) {
 };
 
 // Handle category update on POST.
-exports.category_update_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Category update POST');
-};
+exports.category_update_post = [
+    // validate and sanitise 
+    body('category_name').trim().isLength({ min: 1 }).withMessage('Category name required.').escape(),
+    body('category_desc').trim().isLength({ min: 1 }).withMessage('Category description required.').escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        let new_category = new Category({
+            _id: req.params.id,
+            name: req.body.category_name,
+            description: req.body.category_desc,
+        });
+        // if there are errors render the form again with sanitized values/errors messages
+        if (!errors.isEmpty()) {
+            res.render('category_form', { title: 'Update Category', category: new_category, errors: errors.array() });
+        } else {
+            Category.findOne({ 'name': req.body.category_name })
+                .exec(function (err, found_category) {
+                    if (err) {
+                        return next(err);
+                    }
+                    if (found_category) {
+                        res.redirect(found_category.url)
+                    } else {
+                        Category.findByIdAndUpdate(req.params.id, new_category, {}, function (err, category) {
+                            if (err) {
+                                return next(err);
+                            }
+                            // Successful - redirect to new category.
+                            res.redirect(category.url);
+                        });
+                    }
+                });
+        }
+    }
+];
